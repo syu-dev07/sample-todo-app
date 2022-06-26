aa76d3654db2e6377696772bf1fb0fff6c97a193
syu-dev07 <79366754+syu-dev07@users.noreply.github.com>
2022-06-27 08:06:02 +0900

    スタイル調整

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,15 +1,41 @@
 import React, { useContext } from "react";
 import { todoContext } from "../pages/_app";
 import { Todo } from "./Todo";
+import styles from "./TodoList.module.css";
 
 export const TodoList = () => {
   const { todos } = useContext(todoContext);
   console.log(todos);
   return (
-    <ul>
-      {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} />;
-      })}
-    </ul>
+    <div className={styles.container}>
+      <div className={styles.board}>
+        <h1>未完了</h1>
+        <ul>
+          {todos
+            .filter((todo) => {
+              if (!todo.isDone) {
+                return todo;
+              }
+            })
+            .map((todo) => {
+              return <Todo key={todo.id} todo={todo} />;
+            })}
+        </ul>
+      </div>
+      <div className={styles.board}>
+        <h1>完了</h1>
+        <ul>
+          {todos
+            .filter((todo) => {
+              if (todo.isDone) {
+                return todo;
+              }
+            })
+            .map((todo) => {
+              return <Todo key={todo.id} todo={todo} />;
+            })}
+        </ul>
+      </div>
+    </div>
   );
 };
